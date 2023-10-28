43ae3a31943ccaa908aa942c1f5b17214ef9bbef
Nagy Zétény <nagyzeteny6@gmail.com>
2023-10-28 20:58:54 +0200

    Update switch

--- 04-switch/Switch/Program.cs
+++ 04-switch/Switch/Program.cs
@@ -1,7 +1,8 @@
-﻿// 1. feladat
+﻿var random = new Random();
+
+#region 1. feladat
 Console.Write("Adjon meg egy érdemjegyet: ");
-int grade = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(grade switch
+Console.WriteLine(Convert.ToInt32(Console.ReadLine()) switch
 {
     1 => "Elégtelen",
     2 => "Elégséges",
@@ -10,10 +11,11 @@ Console.WriteLine(grade switch
     5 => "Jeles",
     _ => "Nincs ilyen érdemjegy!"
 });
+#endregion
 
-// 2.feladat
-Console.Write("Adja meg egy hónap nevét: ");
-switch (Console.ReadLine())
+#region 2.feladat
+Console.Write("\nAdja meg egy hónap nevét: ");
+switch ((Console.ReadLine() ?? "").ToLower())
 {
     case "január":
     case "február":
@@ -39,11 +41,11 @@ switch (Console.ReadLine())
         Console.WriteLine("Nincs ilyen hónap!");
         break;
 }
+#endregion
 
-// 3.feladat
-Console.Write("Adja meg egy hónap sorszámát: ");
-int monthAsInt = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(monthAsInt switch
+#region 3.feladat
+Console.Write("\nAdja meg egy hónap sorszámát: ");
+Console.WriteLine(Convert.ToInt32(Console.ReadLine()) switch
 {
     < 1 => "Nincs ilyen hónap",
     <= 2 => "Tél",
@@ -53,11 +55,11 @@ Console.WriteLine(monthAsInt switch
     12 => "Tél",
     _ => "Nincs ilyen hónap"
 });
+#endregion
 
-// 4.feladat
-Console.Write("Adja meg, hogy átlagosan hány órát alszik naponta: ");
-int sleepHours = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(sleepHours switch
+#region 4.feladat
+Console.Write("\nAdja meg, hogy átlagosan hány órát alszik naponta: ");
+Console.WriteLine(Convert.ToInt32(Console.ReadLine()) switch
 {
     < 0 => "Csak természetes számot adhat meg!",
     <= 6 => "Kevés",
@@ -66,32 +68,67 @@ Console.WriteLine(sleepHours switch
     <= 24 => "Nagyon sok",
     _ => "Egy nap 24 órából áll!"
 });
+#endregion
 
-//5. feladat
-Console.Write("Adjon megy egy szeptemberi napot: ");
+#region 5. feladat
+Console.Write("\nAdjon megy egy szeptemberi napot: ");
 int day = Convert.ToInt32(Console.ReadLine());
-DateTime dateValue = new DateTime(2023, 09, day);
+var dateValue = new DateTime(2023, 09, day, 0, 0, 0, DateTimeKind.Utc);
 Console.WriteLine(dateValue.DayOfWeek switch
 {
-    DayOfWeek.Monday => "Matek, Töri, Angol, Német, Irodalom, Tesi",
-    DayOfWeek.Tuesday => "Webprogramozás, IKT",
-    DayOfWeek.Wednesday => "Matek, Fizika, Tesi, Töri, Nyelvtan, Szakmai Angol",
-    DayOfWeek.Thursday => "Linux, Adatbázis, Asztali alkalmazás fejlesztés",
-    DayOfWeek.Friday => "Fizika, Irodalom, Matematika, Osztályfőnöki, Angol, Német, Tesi, Digitális kultúra",
-    _ => "Hétvége"
+    DayOfWeek.Monday => "Hétfő: Matek, Töri, Angol, Német, Irodalom, Tesi",
+    DayOfWeek.Tuesday => "Kedd: Webprogramozás, IKT",
+    DayOfWeek.Wednesday => "Szerda: Matek, Fizika, Tesi, Töri, Nyelvtan, Szakmai Angol",
+    DayOfWeek.Thursday => "Csütörtök: Linux, Adatbázis, Asztali alkalmazás fejlesztés",
+    DayOfWeek.Friday => "Péntek: Fizika, Irodalom, Matematika, Osztályfőnöki, Angol, Német, Tesi, Digitális kultúra",
+    DayOfWeek.Saturday => "Szombat",
+    DayOfWeek.Sunday => "Vasárnap",
+    _ => "Nincs ilyen nap!"
 });
+#endregion
 
-//6. feladat
-Console.WriteLine("Biztos, hogy nem!");
+#region 6. feladat
+Console.Write("\nAdja meg egy osztály jelét: ");
+Console.WriteLine((Console.ReadLine() ?? "").ToLower() switch
+{
+    "9.ny" => "Forray Edina, 35. terem",
+    "9.a" => "Menich Ilona, 120. terem",
+    "9.b" => "Várkonyi Tibor Zoltán, 133. terem",
+    "9.c" => "Tóthné Vers Éva, 44. terem",
+    "9.d" => "Répásné Babucs Hajnalka, 125. terem",
 
-//7. feladat
-Console.Write("Adjon meg egy számot: ");
+    "10.a" => "Szokola Melinda, 129. terem",
+    "10.b" => "Halász-Szabó Lídia Ildikó, 109. terem",
+    "10.c" => "Rabina Roland Richárd, 233. terem",
+    "10.d" => "Dobronayné Csepeti Melinda, 5. terem",
+
+    "11.a" => "Révész Barnabás, 121. terem",
+    "11.b" => "Németh Mónika, 114. terem",
+    "11.c" => "Kiss Renáta, 45. terem",
+    "11.d" => "Erdős Gábor, 33. terem",
+
+    "12.a" => "Gönczy-Szabó Gabriella, 46. terem",
+    "12.b" => "Mező György, 229. terem",
+    "12.c" => "Veresné Murányi Marian, 205. terem",
+    "12.d" => "Kiss Éva Csilla, 225. terem",
+    "12.e" => "Tellér Gina Margit, 113. terem",
+
+    "2/14.a" => "Bálint György, gt11/gt6",
+    "2/14.b" => "Piedl Péter, gt11/gt5",
+
+    _ => "Nincs ilyen osztály!"
+});
+#endregion
+
+#region 7. feladat
+Console.Write("\nAdjon meg egy számot: ");
 int a = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Adjon meg egy számot: ");
 int b = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Adjon meg egy műveleti jelet: ");
-string op = Console.ReadLine();
-Console.WriteLine(op switch
+Console.WriteLine(Console.ReadLine() switch
 {
     "+" => a + b,
     "-" => a - b,
@@ -100,36 +137,73 @@ Console.WriteLine(op switch
     "^" => Math.Pow(a, b),
     _ => "Nincs ilyen operátor!"
 });
+#endregion
 
-//8.feladat
-Console.Write("Adjon meg egy összeget: ");
-string money = Console.ReadLine();
-switch (money[^1])
-{
-    case '0':
-    case '5':
-        Console.WriteLine(money);
-        break;
-    case '1':
-    case '2':
-        Console.WriteLine(money[..^1] + 0);
-        break;
-    case '3':
-    case '4':
-    case '6':
-    case '7':
-        Console.WriteLine(money[..^1] + 5);
-        break;
-    case '8':
-    case '9':
-        Console.WriteLine();
-        break;
-}
+#region 8.feladat
+Console.Write("\nAdjon meg egy összeget: ");
+double amount = Convert.ToDouble(Console.ReadLine());
 
-//9.feladat
-Console.Write("Adjon meg egy évszámot: ");
+Console.WriteLine($"Készpénzben fizetendő összeg: {Convert.ToInt32(Math.Round(amount / 5) * 5):C0}");
+#endregion
+
+#region 9.feladat
+Console.Write("\nAdjon meg egy évszámot: ");
 int year = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Adja meg egy hónap nevét");
-switch (Console.ReadLine())
+Console.WriteLine((Console.ReadLine() ?? "").ToLower() switch
 {
+    "január" => "31",
+    "február" => year % 4 == 0 && year % 100 != 0 ? "29" : "28",
+    "március" => "31",
+    "április" => "30",
+    "május" => "31",
+    "június" => "30",
+    "július" => "31",
+    "augusztus" => "31",
+    "szeptember" => "30",
+    "október" => "31",
+    "november" => "30",
+    "december" => "31",
+    _ => "Nincs ilyen hónap!"
+});
+#endregion
+
+#region 10.feladat
+Console.Write("\nAdja meg a vásárlás összegét: ");
+int spentAmount = Convert.ToInt32(Console.ReadLine());
+string? gift = spentAmount switch
+{
+    > 10000 => "zsákbamacska",
+    > 6000 => "tábla csoki",
+    > 3000 => "gumicukor",
+    _ => null
+};
+
+if (gift == "zsákbamacska")
+    gift = new string[] { "pohárkrém", "kandírozott gyümölcs", "marcipán figura" }[random.Next(3)];
+
+if (gift == "pohárkrém")
+    gift = new string[] { "csokoládé", "vanília", "mogyoró", "puncs" }[random.Next(4)] + " ízű pohárkrém";
+
+Console.WriteLine($"A vásárlás összege: {spentAmount}, " +
+    $"ezért {spentAmount / 500} db nyereményszelvény, " +
+    $"illetve {(gift is null ? "nem jár ajándék" : $"egy ajándék {gift} jár")}.");
+#endregion
+
+#region 11.feladat
+Console.Write("\nAdja meg az időt (óra:perc): ");
+int hour = int.Parse((Console.ReadLine() ?? "").Split(':')[0]);
+
+if (hour is >= 6 and < 21)
+{
+    Console.Write("Elegendő fény szűrődik be (i/n)? ");
+    Console.WriteLine((Console.ReadLine() ?? "") switch
+    {
+        "i" => "A lámpa nem világít.",
+        "n" => "A lámpa nappali fénnyel világít.",
+        _ => "Igennel vagy nemmel válaszoljon!",
+    });
 }
+else Console.WriteLine("A lámpa éjszakai fénnyel világít.");
+#endregion
