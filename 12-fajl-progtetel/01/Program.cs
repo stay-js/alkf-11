e0536a9ff7835f4a62505c855f1b305906e57bf6
Nagy Zétény <nagyzeteny6@gmail.com>
2023-11-09 18:33:47 +0100

    Add fajl-progtetel feladat

---
+++ 12-fajl-progtetel/01/Program.cs
@@ -0,0 +1,102 @@
+﻿#region a, b.
+Console.Write("Adja meg a beolvasandó fájl nevét: ");
+var input = new StreamReader(Console.ReadLine() ?? "");
+int length = Convert.ToInt32(input.ReadLine());
+int[] numbers = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+    numbers[i] = Convert.ToInt32(input.ReadLine());
+    Console.WriteLine($"{i + 1}. szám: {numbers[i]}");
+}
+
+input.Close();
+#endregion
+
+#region c.
+Console.WriteLine($"\n{string.Join("\t", numbers)}");
+#endregion
+
+#region d.
+var output = new StreamWriter("parosaval.txt");
+
+for (int i = 0; i < length - 1; i++)
+{
+    int a = numbers[i];
+    int b = numbers[i + 1];
+    output.WriteLine($"{a};{b};{((a + b) / 2.0):N2}");
+}
+
+output.Close();
+#endregion
+
+#region e.
+double sum = 0;
+
+for (int i = 0; i < length; i++)
+{
+    sum += numbers[i];
+}
+
+Console.WriteLine($"\nA számok összege: {sum}");
+#endregion
+
+#region f.
+Console.WriteLine($"A számok átlaga: {sum / length}");
+#endregion
+
+#region g.
+int max = numbers[0];
+
+for (int i = 1; i < length; i++)
+{
+    if (numbers[i] > max) max = numbers[i];
+}
+
+Console.WriteLine($"A számok maximuma: {max}");
+#endregion
+
+#region h.
+int min = numbers[0];
+
+for (int i = 1; i < length; i++)
+{
+    if (numbers[i] < min) min = numbers[i];
+}
+
+Console.WriteLine($"A számok minimuma: {min}");
+#endregion
+
+#region i.
+int j = 0;
+
+while (j < length && numbers[j] != 0)
+{
+    j++;
+}
+
+Console.WriteLine($"A számok között {(j < length ? "van" : "nincs")} nulla.");
+#endregion
+
+#region j.
+j = 0;
+
+while (j < length && numbers[j] >= 0)
+{
+    j++;
+}
+
+if (j < length) Console.WriteLine($"Az első negaív szám a {j + 1}. helyen áll.");
+else Console.WriteLine("Nincs negatív szám a számok között.");
+#endregion
+
+#region k.
+double countOfPositive = 0;
+
+foreach (int number in numbers)
+{
+    if (number > 0) countOfPositive++;
+}
+
+Console.WriteLine($"A számok {(countOfPositive / length):P2} pozitív.");
+#endregion
---
+++ 12-fajl-progtetel/02/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+
+string[] names = File.ReadAllLines("nevek.txt");
+
+#region a.
+int count = 0;
+
+foreach (string name in names)
+{
+    if (name.Split().Length == 3) count++;
+}
+
+Console.WriteLine($"{count} beolvasott embernek van 2 keresztneve.");
+#endregion
+
+#region b - e.
+string[] sortedNames = StringBubbleSort(names);
+
+var output = new StreamWriter("rendezett.txt");
+
+Console.WriteLine("\nBeolvasott nevek ABC sorrendben:");
+
+foreach (string name in sortedNames)
+{
+    Console.WriteLine(name);
+    output.WriteLine(name);
+}
+
+output.Close();
+#endregion
+
+#region c.
+const int AMOUNT_TO_PICK = 3;
+string[] randomNames = new string[AMOUNT_TO_PICK];
+int k = 0;
+
+while(k < AMOUNT_TO_PICK)
+{
+    string selectedName = names[Random.Shared.Next(names.Length)];
+    if (!Contains(randomNames, k, selectedName)) randomNames[k++] = selectedName;
+}
+
+output = new StreamWriter("felelok.txt");
+output.WriteLine(string.Join("\n", randomNames));
+output.Close();
+#endregion
+
+#region d.
+output = new StreamWriter("nagybetus.txt");
+
+foreach (string name in names)
+{
+    output.WriteLine(name.ToUpper());
+}
+
+output.Close();
+#endregion
+
+#region f.
+string[] firstNames = new string[names.Length];
+int l = 0;
+
+foreach (string name in names)
+{
+    string firstName = string.Join(' ', name.Split()[1..]);
+    if (!Contains(firstNames, l, firstName)) firstNames[l++] = firstName;
+}
+
+string[] firstNamesActualLength = new string[l];
+for (int i = 0; i < l; i++)
+{
+    firstNamesActualLength[i] = firstNames[i];
+}
+
+Console.WriteLine("\nKeresztnevek ABC sorrendben:\n" +
+    string.Join(", ", StringBubbleSort(firstNamesActualLength)));
+#endregion
+
+#region g.
+Console.Write("\nAdjon meg egy monogrammot: ");
+string monogramm = (Console.ReadLine() ?? "").ToUpper();
+
+int m = 0;
+
+while (m < names.Length && string.Concat(names[m].Split().Select((name) => name[0])) != monogramm)
+{
+    m++;
+}
+
+if (m < names.Length) Console.WriteLine(names[m]);
+else Console.WriteLine("Nincs ilyen monogrammal rendelkező ember a listán.");
+
+#endregion
+
+
+static string[] StringBubbleSort(string[] array)
+{
+    string[] sortedArray = array[..];
+
+    for (int i = sortedArray.Length; i >= 1; i--)
+    {
+        for (int j = 0; j < i - 1; j++)
+        {
+            if (string.Compare(sortedArray[j], sortedArray[j + 1]) > 0)
+            {
+                (sortedArray[j], sortedArray[j + 1]) = (sortedArray[j + 1], sortedArray[j]);
+            }
+        }
+    }
+
+    return sortedArray;
+}
+
+static bool Contains(string[] array, int length, string item)
+{
+    int i = 0;
+
+    while (i < length && array[i] != item)
+    {
+        i++;
+    }
+
+   return i < length;
+}
