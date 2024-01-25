2ba25c22a04e1a403d5773ba8efdaaff0a921109
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-25 19:17:04 +0100

    simplify 1 feladat

--- 16-record/01/Program.cs
+++ 16-record/01/Program.cs
@@ -5,29 +5,30 @@ var data = ReadData();
 #endregion
 
 #region b
-var maxScore = data[MaxScore()];
-Console.WriteLine($"A legmadasabb pontszámot a {maxScore.Id} osztály érte el: " +
-                  $"{maxScore.Score} pont.");
+var maxScore = data.MaxBy(x => x.Score) ?? new Class("99.zs", 0);
+Console.WriteLine($"A legmagasabb pontszámot a {maxScore.Id} osztály érte el: " +
+    $"{maxScore.Score} pont.");
 #endregion
 
 #region c
-Console.WriteLine($"Az átlagos pontszám: {AvgScore()} pont.");
+Console.WriteLine($"Az átlagos pontszám: {data.Average(x => x.Score)} pont.");
 #endregion
 
 #region d
-Console.WriteLine($"{CountLessThan100()} osztály ért el 100-nál kevesebb pontot.");
+Console.WriteLine($"{data.Count(x => x.Score < 100)} osztály ért el 100-nál kevesebb pontot.");
 #endregion
 
 #region e
 Console.WriteLine();
-Console.WriteLine(GetScore(out int score)
-    ? $"A keresett oszály {score} pontot ért el."
-    : "A keresett osztály nem található.");
+int? score = GetScoreById();
+Console.WriteLine(score is null
+    ? "A keresett osztály nem található."
+    : $"A keresett oszály {score} pontot ért el.");
 #endregion
 
 #region f
 Console.WriteLine("\nAz alábbi osztályok értek el 50-nél kevesebb pontot:\n\t- " +
-                  string.Join("\n\t- ", SelectLessThen50().Select(x => x.Id)));
+    string.Join("\n\t- ", data.Where(x => x.Score < 50).Select(x => x.Id)));
 #endregion
 
 static Class[] ReadData()
@@ -39,73 +40,17 @@ static Class[] ReadData()
     }).ToArray();
 }
 
-int MaxScore()
-{
-    int max = 0;
-
-    for (int i = 0; i < data.Length; i++)
-    {
-        if (data[i].Score > data[max].Score) max = i;
-    }
-
-    return max;
-}
-
-double AvgScore()
-{
-    double sum = 0;
-
-    foreach (var item in data)
-    {
-        sum += item.Score;
-    }
-
-    return sum / data.Length;
-}
-
-int CountLessThan100()
-{
-    int count = 0;
-
-    foreach (var item in data.Where(x => x.Score < 100))
-    {
-        count++;
-    }
-
-    return count;
-}
-
-bool GetScore(out int score)
+int? GetScoreById()
 {
     Console.Write("Adja meg a keresett osztály azonosítóját: ");
     string id = Console.ReadLine() ?? "";
 
-    score = -1;
-
     foreach (var item in data)
     {
-        if (item.Id != id) continue;
-
-        score = item.Score;
-        return true;
+        if (item.Id == id) return item.Score;
     }
 
-    return false;
-}
-
-Class[] SelectLessThen50()
-{
-    var lessThan50 = new Class[data.Length];
-    int len = 0;
-
-    foreach (var item in data.Where(x => x.Score < 50))
-    {
-        lessThan50[len++] = item;
-    }
-
-    Array.Resize(ref lessThan50, len);
-
-    return lessThan50;
+    return null;
 }
 
 namespace Local
