866684f6926f48ad4c010b15426cf4888fc2537d
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-07 11:49:46 +0100

    add auto feladat

---
+++ 20-OOP-gyakorlas/auto/AutoApp/Auto.cs
@@ -0,0 +1,48 @@
+namespace AutoApp
+{
+    internal class Auto
+    {
+        private readonly List<int> _sebessegek;
+
+        public string Tipus { get; init; }
+        public string Vezeto { get; set; }
+        public int MaxSebesseg { get; init; }
+
+        public double AtlagSebesseg => _sebessegek.Count == 0 ? 0 : _sebessegek.Average();
+
+        public Auto()
+        {
+            _sebessegek = new List<int>();
+            Tipus = "Ismeretlen";
+            Vezeto = "";
+            MaxSebesseg = 100;
+        }
+
+        public Auto(string tipus, string vezeto, int maximalisSebesseg)
+        {
+            _sebessegek = new List<int>();
+            Tipus = tipus;
+            Vezeto = vezeto;
+            MaxSebesseg = maximalisSebesseg < 100 ? 100 : maximalisSebesseg;
+        }
+
+        public int SebessegMeres()
+        {
+            if (Vezeto == "") return 0;
+
+            int sebesseg = Random.Shared.Next(MaxSebesseg / 4, MaxSebesseg);
+            _sebessegek.Add(sebesseg);
+
+            return sebesseg;
+        }
+
+        public string Log() => $"Az autó sofőrje: {Vezeto}, átlagsebessége: {AtlagSebesseg}";
+
+        public int Osszehasonlitas(Auto auto)
+        {
+            if (auto.AtlagSebesseg < AtlagSebesseg) return 1;
+            if (auto.AtlagSebesseg > AtlagSebesseg) return -1;
+            return 0;
+        }
+    }
+}
---
+++ 20-OOP-gyakorlas/auto/AutoApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using AutoApp;
+
+var totoka1 = new Auto();
+Console.WriteLine(totoka1.Log());
+Console.WriteLine(totoka1.Tipus);
+Console.WriteLine(totoka1.SebessegMeres());
+
+var totoka2 = new Auto("Suzuki Swift", "Lázár Boldizsár", 210);
+Console.WriteLine(totoka2.Log());
+
+Console.WriteLine(totoka2.SebessegMeres());
+Console.WriteLine(totoka2.SebessegMeres());
+Console.WriteLine(totoka2.SebessegMeres());
+Console.WriteLine(totoka2.SebessegMeres());
+Console.WriteLine(totoka2.SebessegMeres());
+
+Console.WriteLine(totoka2.Log());
+
+var totoka3 = new Auto("Suzuki Wagon R+", "Révész Barnabás", 90);
+Console.WriteLine(totoka3.MaxSebesseg);
+
+Console.WriteLine(totoka3.SebessegMeres());
+Console.WriteLine(totoka3.SebessegMeres());
+Console.WriteLine(totoka3.SebessegMeres());
+Console.WriteLine(totoka3.SebessegMeres());
+Console.WriteLine(totoka3.SebessegMeres());
+
+Console.WriteLine(totoka3.Log());
+
+Console.WriteLine(totoka2.Osszehasonlitas(totoka3));
