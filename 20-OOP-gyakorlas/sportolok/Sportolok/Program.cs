7a8cb714e4f14349882725f1f75377ba225dcfe8
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-21 19:26:59 +0100

    add sportolok feladat

---
+++ 20-OOP-gyakorlas/sportolok/Sportolok/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sportolok;
+
+var sportolok = new List<Sportolo>();
+
+while (true)
+{
+    Console.Write("Adja meg az új sportoló nevét (kilépéshez üres enter): ");
+    string input = Console.ReadLine() ?? "";
+    if (input == "") break;
+
+    sportolok.Add(new Sportolo(input));
+}
+
+foreach (var sportolo in sportolok)
+{
+    Console.WriteLine($"\n{sportolo.Nev} eredményei:");
+
+    while (true)
+    {
+        Console.Write("Adja meg az eredményt (kilépéshez üres enter): ");
+        string input = Console.ReadLine() ?? "";
+        if (input == "") break;
+
+        sportolo.UjEredmeny(int.Parse(input));
+    }
+}
+
+Console.WriteLine($"\n\nSportolók:\n{string.Join('\n', sportolok)}");
+Console.WriteLine($"\nVilágrekord: {Sportolo.WR}");
+Console.WriteLine($"Rekordtartó: {sportolok.Find(s => s.PB == Sportolo.WR)!.Nev}");
---
+++ 20-OOP-gyakorlas/sportolok/Sportolok/Sportolok.cs
@@ -0,0 +1,51 @@
+﻿namespace Sportolok
+{
+    internal enum Kategoria
+    {
+        Amator,
+        Profi,
+        Klasszis
+    }
+
+    internal class Sportolo
+    {
+        public static int WR { get; private set; } = 0;
+
+        private readonly int[] _eredmenyek = new int[20];
+        private int _eszam = 0;
+
+        public string Nev { get; init; }
+
+        public int PB => _eredmenyek.Max();
+
+        private static void SetWR(int record) => WR = record;
+
+        public Sportolo(string nev) => Nev = nev;
+
+        public Kategoria Kategoria
+        {
+            get
+            {
+                if (_eszam == 0) return Kategoria.Amator;
+                if (PB < WR * 0.3) return Kategoria.Amator;
+                if (PB < WR * 0.75) return Kategoria.Profi;
+                return Kategoria.Klasszis;
+            }
+        }
+
+        public void UjEredmeny(int eredmeny)
+        {
+            if (_eszam >= 20) return;
+
+            _eredmenyek[_eszam++] = eredmeny;
+            if (eredmeny > WR) SetWR(eredmeny);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} - eredmények: " +
+                (_eszam == 0 ? "-" : string.Join(", ", _eredmenyek[0.._eszam])) +
+                $"; legjobb eredmény: {PB}; kategória: {Kategoria}";
+        }
+    }
+}
