b4d506f99b2d4daff798e3f16ed641e136055699
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-21 18:21:39 +0100

    add repa feladat

---
+++ 20-OOP-gyakorlas/repa/RepApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using RepApp;
+
+var repa = new Repa();
+Console.WriteLine(repa);
+
+Console.WriteLine(repa.Tick(23, 0.5));
+Console.WriteLine(repa);
+
+var repa2 = new Repa("teszt");
+Console.WriteLine(repa2);
+
+Console.WriteLine(repa2.Tick(43, 0.75));
+Console.WriteLine(repa2);
+
+var repa3 = new Repa();
+Console.WriteLine(repa3);
+
+repa3.Minoseg = 8;
+Console.WriteLine(repa3);
+
+repa3.Minoseg = -1;
+Console.WriteLine(repa3);
+
+repa3.Minoseg = 1.0 / 3.0;
+Console.WriteLine(repa3);
---
+++ 20-OOP-gyakorlas/repa/RepApp/Repa.cs
@@ -0,0 +1,33 @@
+﻿namespace RepApp
+{
+    internal class Repa
+    {
+        private readonly int _tapErtek = Random.Shared.Next(1, 6);
+        private double _minoseg = 0.99;
+
+        public int Kor { get; private set; } = 0;
+        public double TapErtek => _tapErtek * _minoseg;
+
+        public double Minoseg
+        {
+            get => _minoseg;
+            set => _minoseg = 0 < value && value < 1 ? value : _minoseg;
+        }
+
+        public string Fajta { get; init; }
+
+        public Repa() => Fajta = new string[] { "sárgarépa", "fehérrépa", "cukorrépa" }[Random.Shared.Next(0, 2)];
+
+        public Repa(string fajta) => Fajta = fajta;
+
+        public override string ToString() => $"{Fajta} ({Kor} éves) - {TapErtek}";
+
+        public bool Tick(int korNoveles, double minosegCsokkentes)
+        {
+            Kor += korNoveles;
+            _minoseg -= minosegCsokkentes;
+
+            return Kor < 30 && _minoseg > 0;
+        }
+    }
+}
