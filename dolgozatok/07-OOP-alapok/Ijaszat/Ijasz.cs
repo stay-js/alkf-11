85e97bcfc06508006a98f9ebd4f5560a74bf6a1e
Nagy Zétény <nagyzeteny6@gmail.com>
2024-04-11 17:40:35 +0200

    Add oop alapok dolgozat

---
+++ dolgozatok/07-OOP-alapok/Ijaszat/Ijasz.cs
@@ -0,0 +1,71 @@
+﻿namespace Ijaszat
+{
+    internal class Ijasz
+    {
+        private const int _TP = 10;
+        private int _alloKepesseg;
+        private int _ugyesseg;
+        private int _tapasztalat = 0;
+
+        public static bool AkaratEro => Random.Shared.Next(1, 11) == 1;
+
+        public int Szint { get; private set; } = 1;
+
+        public int AlloKepesseg
+        {
+            get => _alloKepesseg;
+            set
+            {
+                if (value < 0) _alloKepesseg = 0;
+                else if (value > 100) _alloKepesseg = 100;
+                else _alloKepesseg = value;
+            }
+        }
+
+        public int Ugyesseg
+        {
+            get => _ugyesseg;
+            set
+            {
+                if (value < 0) _ugyesseg = 0;
+                else if (value > 10) _ugyesseg = 10;
+                else _ugyesseg = value;
+            }
+        }
+
+        public Ijasz()
+        {
+            AlloKepesseg = Random.Shared.Next(30, 61);
+            Ugyesseg = 1;
+        }
+
+        public Ijasz(int ugyesseg)
+        {
+            AlloKepesseg = Random.Shared.Next(50, 76);
+            Ugyesseg = ugyesseg;
+        }
+
+        public bool Lo()
+        {
+            if (AlloKepesseg < 5 && !AkaratEro) return false;
+
+            AlloKepesseg -= 5;
+            _tapasztalat += Szint * Ugyesseg;
+
+            if (_tapasztalat > _TP * Szint * Szint)
+            {
+                Szint++;
+                _tapasztalat = 0;
+
+                if (Szint % 3 == 0) Ugyesseg++;
+            }
+
+            return true;
+        }
+
+        public void Pihen() => AlloKepesseg += Random.Shared.Next(1, 11);
+
+        public string Info() => $"{Szint}. szintű íjász. Ügyessége: {Ugyesseg} Állóképessége: {AlloKepesseg}";
+        public override string ToString() => Info();
+    }
+}
---
+++ dolgozatok/07-OOP-alapok/Ijaszat/Program.cs
@@ -0,0 +1,61 @@
+﻿using Ijaszat;
+
+var tesztIjasz = new Ijasz(-1);
+Console.WriteLine(tesztIjasz);
+
+tesztIjasz = new Ijasz(1200);
+Console.WriteLine(tesztIjasz);
+
+tesztIjasz.AlloKepesseg = -12;
+Console.WriteLine(tesztIjasz);
+
+tesztIjasz.AlloKepesseg = 1013;
+Console.WriteLine(tesztIjasz + "\n");
+
+var log = new StreamWriter("log.txt");
+
+var kezdoIjasz = new Ijasz();
+Console.WriteLine(kezdoIjasz);
+log.WriteLine(kezdoIjasz);
+
+var ugyesIjasz = new Ijasz(Random.Shared.Next(3, 6));
+Console.WriteLine(ugyesIjasz);
+log.WriteLine(ugyesIjasz);
+
+while (true)
+{
+    Console.Write("\nVálassza ki az íjászt (k/u): ");
+    string selection = Console.ReadLine() ?? "";
+
+    if (selection == "k") HandleArcher(kezdoIjasz);
+    else if (selection == "u") HandleArcher(ugyesIjasz);
+    else break;
+}
+
+log.Close();
+
+
+void HandleArcher(Ijasz archer)
+{
+    Console.Write("Válassza ki, hogy mit csináljon az íjász (l/p): ");
+    string selection = Console.ReadLine() ?? "";
+    string toLog = "";
+
+    if (selection == "l")
+    {
+        toLog = archer.Lo()
+            ? "Az íjász ellőtte a nyilat."
+            : "Az íjász túl fáradt, pihennie kell.";
+    }
+    else if (selection == "p")
+    {
+        archer.Pihen();
+        toLog = "Az íjász pihent.";
+    }
+
+    Console.WriteLine(toLog);
+    log.WriteLine(toLog);
+
+    Console.WriteLine(archer);
+    log.WriteLine(archer);
+}
