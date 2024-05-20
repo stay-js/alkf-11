624266e2bf3f6199e5b0920b79b1b491419ef631
Nagy Zétény <nagyzeteny6@gmail.com>
2024-05-20 15:48:20 +0200

    Add datetime timespan feladat

---
+++ 23-datetime-timespan/02/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Globalization;
+
+Console.Write("Adja meg a születési dátumát: ");
+var dateOfBirth = DateTime.Parse(Console.ReadLine() ?? "", new CultureInfo("hu-HU"));
+
+Console.WriteLine($"\t- {Math.Floor((DateTime.UtcNow - dateOfBirth).TotalDays / 365.25)} éves");
+Console.WriteLine($"\t- {Math.Floor((DateTime.UtcNow - dateOfBirth).TotalDays)} napos");
+Console.WriteLine($"\t- {dateOfBirth.DayOfWeek} napon született");
+Console.WriteLine($"\t- Idén {dateOfBirth.AddYears(1).DayOfWeek} napon lesz a születésnapja");
+Console.WriteLine($"\t- Csillagjegye: {ZodiacSign()}");
+
+string ZodiacSign()
+{
+    if (dateOfBirth.Month == 12)
+    {
+        if (dateOfBirth.Day < 22) return "Nyilas";
+        return "Bak";
+    }
+    else if (dateOfBirth.Month == 1)
+    {
+        if (dateOfBirth.Day < 20) return "Bak";
+        return "Vízöntő";
+    }
+    else if (dateOfBirth.Month == 2)
+    {
+        if (dateOfBirth.Day < 19) return "Vízöntő";
+        return "Halak";
+    }
+    else if (dateOfBirth.Month == 3)
+    {
+        if (dateOfBirth.Day < 21) return "Halak";
+        return "Kos";
+    }
+    else if (dateOfBirth.Month == 4)
+    {
+        if (dateOfBirth.Day < 20) return "Kos";
+        return "Bika";
+    }
+    else if (dateOfBirth.Month == 5)
+    {
+        if (dateOfBirth.Day < 21) return "Bika";
+        return "Ikrek";
+    }
+    else if (dateOfBirth.Month == 6)
+    {
+        if (dateOfBirth.Day < 21) return "Ikrek";
+        return "Rák";
+    }
+    else if (dateOfBirth.Month == 7)
+    {
+        if (dateOfBirth.Day < 23) return "Rák";
+        return "Oroszlán";
+    }
+    else if (dateOfBirth.Month == 8)
+    {
+        if (dateOfBirth.Day < 23) return "Oroszlán";
+        return "Szűz";
+    }
+    else if (dateOfBirth.Month == 9)
+    {
+        if (dateOfBirth.Day < 23) return "Szűz";
+        return "Mérleg";
+    }
+    else if (dateOfBirth.Month == 10)
+    {
+        if (dateOfBirth.Day < 23) return "Mérleg";
+        return "Skorpió";
+    }
+    else
+    {
+        if (dateOfBirth.Day < 22) return "Skorpió";
+        return "Nyilas";
+    }
+}
---
+++ 23-datetime-timespan/03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+Console.Write("Adjon meg egy dátumot: ");
+var date1 = DateTime.Parse(Console.ReadLine() ?? "", new CultureInfo("hu-HU"));
+
+Console.Write("Adjon meg egy másik dátumot: ");
+var date2 = DateTime.Parse(Console.ReadLine() ?? "", new CultureInfo("hu-HU"));
+
+Console.WriteLine();
+
+if (date1 < date2) Console.WriteLine("Az első dátum korábbi.");
+else if (date1 > date2) Console.WriteLine("A második dátum korábbi.");
+else Console.WriteLine("A két dátum megegyezik.");
+
+Console.WriteLine($"{date1.Year} {(DateTime.IsLeapYear(date1.Year) ? "" : "nem ")}szökőév.");
+Console.WriteLine($"{date2.Year} {(DateTime.IsLeapYear(date2.Year) ? "" : "nem ")}szökőév.");
+
+Console.WriteLine($"{date1.ToShortDateString()} => {Season(date1.Month)}");
+Console.WriteLine($"{date2.ToShortDateString()} => {Season(date2.Month)}");
+
+static string Season(int month)
+{
+    return month switch
+    {
+        12 or <= 2 => "Tél",
+        <= 5 => "Tavasz",
+        <= 8 => "Nyár",
+        <= 11 => "Ősz",
+        _ => "Ismeretlen"
+    };
+}
